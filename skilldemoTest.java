f9c31e3ed462dc77a8a002b4fe48bc56443da000
ZiyuWang0113 <ziw003@ucsd.edu>
2022-02-03 19:18:01 -0800

    fixed test and pass

--- skilldemoTest.java
+++ skilldemoTest.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class skilldemoTest {
     @Test
     public void testMulti(){
-        assertEquals(6, skilldemo.multiply(3, 3)); // fail first
+        assertEquals(9, skilldemo.multiply(3, 3)); // pass
     }
 }
